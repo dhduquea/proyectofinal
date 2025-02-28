f54ad520fca77bdb9d9ceb19e238aace62bf2fa0
PabloChavarria <pablocha18@gmail.com>
2025-02-28 15:36:50 -0500

    Se agregan las imágenes de los dashboards BI

--- pages/viento.py
+++ pages/viento.py
@@ -39,5 +39,26 @@ with col2:
     La mayoría de los municipios analizados registran velocidades inferiores a 4 m/s, lo que puede limitar la viabilidad económica de proyectos de energía eólica a gran escala. Sin embargo, podrían considerarse soluciones eólicas de pequeña escala para aplicaciones específicas, como el bombeo de agua o sistemas híbridos en zonas rurales.
     """)
     st.image('media/barplot_viento_trim.jpeg', caption='Velocidad promedio del viento en Antioquia', use_container_width=True)
-   
-
+    st.write("""
+             \n \n
+             """)
+    st.image('media/anual-viento-BI.png', 
+             caption='Velocidad máxima del viento anual en Antioquia', 
+             use_container_width=True)
+    st.write("""
+            En el gráfico generado por un dashboard en Power BI, se puede observar el comportamiento anual
+            de la velocidad del viento en cada municipio, usando una escala de intensidad de color en el 
+            mapa para identificar patrones regionales de la medición.\n
+            Se detectan varios picos anormales en la velocidad del viento, lo que sugiere la presencia de
+            mediciones defectuosas en los sensores de las estaciones meteorológicas.
+            """)
+    st.image('media/diario-viento-BI.png',
+            caption='Velocidad máxima del viento por día en cada municipio',
+            use_container_width=True)
+    st.write("""
+            En el gráfico generado por un dashboard en Power BI, se puede observar el comportamiento de los
+            valores de velocidad del viento máxima por día en cada municipio, usando un gráfico de líneas 
+            para detectar patrones mensuales de comportamiento del viento.\n
+            Se observan similitudes en algunas lineas de comportamiento pero no de manera significativa que 
+            permita establecer un patrón regional.
+             """)
